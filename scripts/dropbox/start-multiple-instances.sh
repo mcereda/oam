2f96c1c070b51ef142eca0f4a32be3b7f770ca8b
Michele Cereda <michele.cereda@protonmail.com>
2021-10-16 23:43:31 +0200

    Fix tabs vs spaces

--- scripts/dropbox/start-multiple-instances.sh
+++ scripts/dropbox/start-multiple-instances.sh
@@ -4,5 +4,5 @@ DROPBOX_ACCOUNTS="${DROPBOX_ACCOUNTS:-private work}"
 
 for ACCOUNT in $DROPBOX_ACCOUNTS
 do
-    HOME="${HOME}/.dropbox-${ACCOUNT}" dropbox start &
+	HOME="${HOME}/.dropbox-${ACCOUNT}" dropbox start &
 done
