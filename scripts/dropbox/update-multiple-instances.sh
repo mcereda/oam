8a599c82e171edc61c4988a5696ae7492d351ea0
Michele Cereda <michele.cereda@protonmail.com>
2021-10-16 23:43:31 +0200

    Fix tabs vs spaces

--- scripts/dropbox/update-multiple-instances.sh
+++ scripts/dropbox/update-multiple-instances.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env sh
 
 function dropbox-install {
-    export DROPBOX_archive="dropbox_daemon.tar.gz"
-    export DROPBOX_retries="3"
-    export DROPBOX_url="http://www.getdropbox.com/download?plat=lnx.x86_64"
+	export DROPBOX_archive="dropbox_daemon.tar.gz"
+	export DROPBOX_retries="3"
+	export DROPBOX_url="http://www.getdropbox.com/download?plat=lnx.x86_64"
 
-    # download daemon
-    echo "  downloading archive…"
-    curl -C - -o $DROPBOX_archive --retry $DROPBOX_retries -S -L $DROPBOX_url
+	# download daemon
+	echo "  downloading archive…"
+	curl -C - -o $DROPBOX_archive --retry $DROPBOX_retries -S -L $DROPBOX_url
 
-    # install daemon
-    echo "  unarchiving tarball…"
-    tar zxf $DROPBOX_archive -C $HOME
+	# install daemon
+	echo "  unarchiving tarball…"
+	tar zxf $DROPBOX_archive -C $HOME
 
-    # cleaning
-    rm $DROPBOX_archive
+	# cleaning
+	rm $DROPBOX_archive
 }
 
 if [ ! -f start-multiple-instances.sh ]
 then
-    echo "[ERROR] Dropbox multi-instances start script not usable. Aborting."
-    exit 1
+	echo "[ERROR] Dropbox multi-instances start script not usable. Aborting."
+	exit 1
 fi
 
 if [ ! -d ${HOME}/.dropbox-dist ]
 then
-    echo "[WARNING] Default Dropbox dist directory not found."
-    echo "[WARNING] Downloading and installing in the default directory."
+	echo "[WARNING] Default Dropbox dist directory not found."
+	echo "[WARNING] Downloading and installing in the default directory."
 else
-    rm -r ${HOME}/.dropbox-dist
+	rm -r ${HOME}/.dropbox-dist
 fi
 
 echo "[NOTICE] Stopping all processes using the current version of Dropbox."
