0eedb247ee3b91abefd3f26c0ea9006e7faa6094
Michele Cereda <cereda.michele@gmail.com>
2022-04-17 23:12:11 +0200

    Fixed typos, titles and spacing

--- scripts/opensuse/dropbox.install.sh
+++ scripts/opensuse/dropbox.install.sh
@@ -8,4 +8,4 @@
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 cd bin && wget -O dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py" && chmod u+x dropbox.py && cd -
-dropbox.py start  # or ~/.dropbox-dist/dropboxd
+dropbox.py start   # or ~/.dropbox-dist/dropboxd
