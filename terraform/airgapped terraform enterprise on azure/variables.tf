cf3d57046a6e0d19c1135010a66734c605b5c793
Michele Cereda <cereda.michele@gmail.com>
2023-01-17 23:21:41 +0100

    Added type constraint

--- terraform/airgapped terraform enterprise on azure/variables.tf
+++ terraform/airgapped terraform enterprise on azure/variables.tf
@@ -1,7 +1,9 @@
 variable "replicated_config_file_path" {
+  type        = string
   default     = "/etc/replicated.conf"
   description = "Only read on initial startup."
 }
 variable "tfe_config_file_path" {
-  default = "/etc/ptfe.settings.json"
+  type    = string
+  default = "/etc/settings.conf"
 }
