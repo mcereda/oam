a7af61a87aaf44d3bebf093643ff0094b6e32454
Michele Cereda <cereda.michele@gmail.com>
2023-01-22 22:44:47 +0100

    Improved requirements list and graph

--- examples/terraform/enterprise/airgapped terraform enterprise on azure/design/requirements.py
+++ examples/terraform/enterprise/airgapped terraform enterprise on azure/design/requirements.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
 
-from diagrams import Diagram
-from diagrams.azure.compute import VMLinux
+from diagrams import Cluster, Diagram
+from diagrams.azure.compute import OsImages, VMLinux
 from diagrams.azure.database import CacheForRedis, DatabaseForPostgresqlServers
-from diagrams.azure.network import LoadBalancers, NetworkInterfaces, Subnets, VirtualNetworks
+from diagrams.azure.network import ApplicationGateway, NetworkInterfaces, VirtualNetworks
 from diagrams.azure.security import KeyVaults
 from diagrams.azure.storage import BlobStorage
+from diagrams.custom import Custom
+from diagrams.generic.os import RedHat
 from diagrams.onprem.container import Docker
 from diagrams.onprem.iac import Terraform
 
 with Diagram("Requirements", show=False):
-
-    cache = CacheForRedis("Redis Cache")
-    db = DatabaseForPostgresqlServers("PostgreSQL DB")
-    engine = Docker("Docker Engine")
-    kv = KeyVaults("Key Vault")
-    lb = LoadBalancers("Load Balancer")
-    nic = NetworkInterfaces("Network Interface")
-    storage = BlobStorage("Blob Storage")
-    subnet_private = Subnets("Private Subnet")
-    subnet_public = Subnets("Public Subnet")
+    container_engine = Docker("Docker Engine")
+    replicated = Custom("Replicated", icon_path = "images/replicated.png")
+    rhel = RedHat("RHEL")
     tfe = Terraform("Terraform Enterprise")
-    vm = VMLinux("Linux Virtual Machine")
-    vnet = VirtualNetworks("VNet")
 
-    vnet >> [subnet_private, subnet_public]
-    subnet_private >> kv
-    [subnet_private, subnet_public] >> nic
-    kv >> [cache, db, storage, vm]
+    with Cluster("Azure"):
+        os = OsImages("Image")
+        vnet = VirtualNetworks("VNet")
+
+        with Cluster("Private Subnet"):
+            cache = CacheForRedis("Redis Cache")
+            db = DatabaseForPostgresqlServers("PostgreSQL DB")
+            kv = KeyVaults("Key Vault")
+            nic = NetworkInterfaces("Network Interface")
+            storage = BlobStorage("Blob Storage")
+            vm = VMLinux("Linux Virtual Machine")
+
+        with Cluster("Public Subnet"):
+            lb = ApplicationGateway("Application Gateway")
+
+    vnet >> [kv, nic]
+    kv >> [cache, db, lb, storage, vm]
     nic >> [lb, vm]
-    vm >> engine
-    [cache, db, engine, lb, storage] >> tfe
+    rhel >> os >> vm
+    storage >> vm >> container_engine >> replicated
+    [cache, db, lb, replicated] >> tfe
