012e13da9f19762d92f73d7b347601619e051b37
Michele Cereda <cereda.michele@gmail.com>
2023-05-09 00:21:46 +0200

    fix: variable quoting

--- examples/move-to-zsh.sh
+++ examples/move-to-zsh.sh
@@ -3,7 +3,7 @@
 sudo pamac install fzf oh-my-zsh zsh zsh-autosuggestions zsh-syntax-highlighting
 # sudo dnf install zsh-syntax-highlighting zsh-autosuggestions fzf
 
-chsh --shell $(which zsh) $USER
+chsh --shell "$(which zsh)" "$USER"
 cp --backup /usr/share/oh-my-zsh/zshrc ~/.zshrc
 
 sed -Ei 's/#*\s*(ZSH_THEME)=.*/\1="refined"/' ~/.zshrc
