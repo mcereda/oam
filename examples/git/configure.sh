137ab0a78af197df7769f3cc7ed48ba64d61a8c5
Michele Cereda <cereda.michele@gmail.com>
2023-05-09 00:20:56 +0200

    cleanup: unused script

--- examples/git/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git config --local user.email name.surname@company.com
-git config --local user.name "Name Surname"
-git config --local user.signingkey 99C324BA
-git config --local commit.gpgsign true
-git config --local pull.rebase false
