a7bf416580097865f94b04a6aef76642993fdd9c
Michele Cereda <cereda.michele@gmail.com>
2024-04-21 05:18:01 +0200

    chore(gitlab): improve installation commands

--- snippets/gitlab.omnibus.reconfigure.sh
+++ snippets/gitlab.omnibus.reconfigure.sh
@@ -1,6 +1,12 @@
 #!sh
 
+# Updated config template available at
+# https://gitlab.com/gitlab-org/omnibus-gitlab/blame/master/files/gitlab-config-template/gitlab.rb.template
+
+# Local template (corresponding to the installed version) available at '/opt/gitlab/etc/gitlab.rb.template'
+
+sudo dnf -y install 'ruby' 'vim'
 sudo vim '/etc/gitlab/gitlab.rb'
-sudo gitlab-ctl check-config
-sudo gitlab-ctl diff-config   # if one really needs to
+sudo ruby -c '/etc/gitlab/gitlab.rb'
+sudo gitlab-ctl show-config
 sudo gitlab-ctl reconfigure
