c456f870bac7518ac65be08f01b18fb5680e0bff
Michele Cereda <cereda.michele@gmail.com>
2024-04-23 18:15:58 +0200

    chore(gpg): improve gpg-agent command selection

--- snippets/gpg.fish
+++ snippets/gpg.fish
@@ -2,6 +2,7 @@
 
 gpg-connect-agent reloadagent '/bye'
 
-gpg-connect-agent updatestartuptty '/bye' \
+gpgconf --launch gpg-agent \
+&& gpg-connect-agent updatestartuptty '/bye' \
 && set -x 'SSH_AUTH_SOCK' (gpgconf --list-dirs 'agent-ssh-socket') \
 && set -x 'GPG_TTY' (tty)
