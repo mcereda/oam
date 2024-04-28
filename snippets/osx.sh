cc8b8d7cceb41fd8717980b4127cd8ec7f9bd918
Michele Cereda <cereda.michele@gmail.com>
2024-04-28 21:09:45 +0200

    chore(osx): add commands to check the dns

--- snippets/osx.sh
+++ snippets/osx.sh
@@ -1,5 +1,9 @@
 #!sh
 
+# Set the host's name.
 scutil --set 'ComputerName' "$(defaults read '/Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName')"
 scutil --set 'HostName' "$(defaults read '/Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName')"
 scutil --set 'LocalHostName' "$(defaults read '/Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName')"
+
+# Clear the DNS cache.
+sudo dscacheutil -flushcache; sudo killall -HUP 'mDNSResponder'
