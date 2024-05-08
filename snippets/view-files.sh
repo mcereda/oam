ce9ba5daede4e61a831bfadeffc718e6c84161bf
Michele Cereda <cereda.michele@gmail.com>
2024-05-08 18:52:34 +0200

    chore(snippets): improve readibility

---
+++ snippets/view-files.sh
@@ -0,0 +1,7 @@
+#!sh
+
+# Just `vim` but read-only.
+view 'file'
+
+# Like `cat` but with syntax highlighting
+bat --theme 'Solarized (light)' 'index.ts'
--- snippets/view.sh
+++
@@ -1,4 +0,0 @@
-#!sh
-
-# Just `vim` but read-only.
-view 'file'
