52f0b2a375ccb0ace96101c277f9d70ef4693b6f
Michele Cereda <cereda.michele@gmail.com>
2024-05-10 17:08:05 +0200

    chore(snippets/gnupg): add export commands

--- snippets/gnupg.fish
+++ snippets/gnupg.fish
@@ -6,3 +6,10 @@ gpgconf --launch gpg-agent \
 && gpg-connect-agent updatestartuptty '/bye' \
 && set -x 'SSH_AUTH_SOCK' (gpgconf --list-dirs 'agent-ssh-socket') \
 && set -x 'GPG_TTY' (tty)
+
+# Export public keys.
+gpg --armor --export 'E455…50AB' | pbcopy
+gpg --export-ssh-key 'E455…50AB' | pbcopy
+
+# Load identities in SSH.
+gpgconf --launch gpg-agent
