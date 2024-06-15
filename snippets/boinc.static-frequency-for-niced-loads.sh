97b37c557e9b21b566eec23afb69341e331d7f33
Michele Cereda <cereda.michele@gmail.com>
2024-06-16 00:08:47 +0200

    chore(boinc/snippets): add command to disable intel turbo boost

--- snippets/boinc.static-frequency-for-niced-loads.sh
+++ snippets/boinc.static-frequency-for-niced-loads.sh
@@ -12,5 +12,13 @@ sudo gpasswd -a boinc video
 xhost si:localuser:boinc
 
 sudo cpupower frequency-set --governor ondemand
-echo 1 | sudo tee /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load
+
+# Do not boost the CPU frequency for niced loads.
+# The governor must support it - check if the file exists.
+# Usually AMD only
+echo 1 | sudo tee '/sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load'
+
+# Disable (1) turbo boost for Intel CPUs.
+echo 1 | sudo tee '/sys/devices/system/cpu/intel_pstate/no_turbo'
+
 sudo systemctl start boinc-client.service
