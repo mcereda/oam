0d1c9bf8f925b816fa57c4599d19e7e57afffc19
Michele Cereda <cereda.michele@gmail.com>
2024-06-26 22:39:23 +0200

    chore(docker): add used commands

--- snippets/docker.sh
+++ snippets/docker.sh
@@ -8,3 +8,5 @@ docker volume inspect -f '{{ .Mountpoint }}' 'website'
 sudo vim '/var/lib/docker/volumes/website/_data/index.html'
 
 docker run -d --name 'some-nginx' -v '/some/content:/usr/share/nginx/html:ro' 'nginx'
+
+docker logs --since '5m' -f 'dblab_server'
