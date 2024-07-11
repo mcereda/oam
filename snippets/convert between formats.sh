3408ef7ef7bf8038ef4665a07a0d754e4bfaa5b6
Michele Cereda <cereda.michele@gmail.com>
2024-07-11 03:04:45 +0200

    chore(snippet): add conversion from webp to gif

--- snippets/convert between formats.sh
+++ snippets/convert between formats.sh
@@ -11,6 +11,9 @@ ffmpeg -y -i 'rec.webm' -i 'palette.png' -filter_complex 'paletteuse' -r 10 'out
 ffmpeg -i 'input.webm' -c 'copy' 'output.mp4'
 ffmpeg -fflags '+genpts' -r '24' -i 'input.webm' 'output.mp4'
 
+# webp to gif
+magick -delay '10' -dispose 'none' 'input.webp' -coalesce -loop '0' -layers 'optimize' 'output.gif'
+
 # webp to png
 dwebp 'input.webp' -o 'output.png'
 dwebp 'input.webp' -mt -o 'output.png' -resize '192' '192'
