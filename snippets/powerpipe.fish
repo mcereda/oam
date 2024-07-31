f720dc0e16b316bcc5f1fe3b0d489e9ee82c476f
Michele Cereda <cereda.michele@gmail.com>
2024-07-31 22:36:24 +0200

    feat(snippets): turbot pipe commands dump

---
+++ snippets/powerpipe.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+brew install 'turbot/tap/powerpipe'
+
+powerpipe --version
+powerpipe start
+
+powerpipe mod init
+powerpipe mod list
+
+powerpipe mod show local
+powerpipe mod show 'steampipe-mod-aws-insights'
+powerpipe mod show 'github.com/turbot/steampipe-mod-aws-insights@v0.21.0'
+
+powerpipe mod install 'github.com/turbot/steampipe-mod-aws-thrifty'
+powerpipe mod install 'github.com/turbot/steampipe-mod-aws-compliance@^0.92'
+powerpipe mod install 'github.com/turbot/steampipe-mod-aws-compliance@^1'
+powerpipe mod install --dry-run 'steampipe-mod-aws-compliance'
+powerpipe mod install --dry-run 'github.com/turbot/steampipe-mod-aws-compliance@v0.93.0' 'github.com/turbot/steampipe-mod-aws-insights'
+
+powerpipe benchmark list
+
+powerpipe plugin install 'aws_compliance.benchmark.soc2'
+
+powerpipe benchmark run 'aws_compliance.benchmark.soc2'
+powerpipe benchmark run 'aws_compliance.benchmark.gdpr' --export 'json' --export 'nunit3'
+
+powerpipe control list
+
+powerpipe control run 'aws_compliance.control.cis_v150_3_3'
+powerpipe control run 'aws_compliance.control.cis_v150_3_3' 'aws_compliance.control.vpc_vpn_tunnel_up'
+
+powerpipe query list
+
+powerpipe query run aws_insights.query.vpc_vpcs_for_vpc_subnet
+powerpipe query run --output table aws_insights.query.vpc_vpcs_for_vpc_subnet
---
+++ snippets/steampipe.fish
@@ -0,0 +1,37 @@
+#!/usr/bin/env fish
+
+brew install 'turbot/tap/steampipe'
+
+steampipe --version
+steampipe service status
+steampipe service start
+steampipe service restart
+steampipe service stop
+
+steampipe plugin list
+steampipe plugin install 'steampipe' 'aws@^0' 'theapsgroup/gitlab@v0.6.0'
+steampipe plugin install 'steampipe' 'aws^0' 'theapsgroup/gitlab@v0.6.0'
+steampipe plugin install 'steampipe' 'aws^0.130' 'theapsgroup/gitlab@v0.6.0'
+steampipe plugin install 'steampipe' 'aws@^0.130' 'theapsgroup/gitlab@v0.6.0'
+steampipe plugin update --all
+steampipe plugin update 'steampipe' 'aws'
+steampipe plugin uninstall 'hub.steampipe.io/plugins/turbot/aws@^0.130'
+steampipe plugin uninstall 'steampipe' 'theapsgroup/gitlab@0.6.0' 'hub.steampipe.io/plugins/turbot/aws@^0'
+
+powerpipe mod install 'github.com/turbot/steampipe-mod-aws-compliance@^0.92'
+powerpipe mod install 'github.com/turbot/steampipe-mod-aws-compliance@^1'
+powerpipe mod install --dry-run 'github.com/turbot/steampipe-mod-aws-compliance'
+powerpipe mod show 'steampipe-mod-aws-insights'
+powerpipe mod show 'github.com/turbot/steampipe-mod-aws-insights@v0.21.0'
+powerpipe mod show github.com/turbot/steampipe-mod-aws-insights@0.21.0
+powerpipe mod show github.com/turbot/steampipe-mod-aws-insights
+
+steampipe check all
+steampipe check mod
+steampipe check 'benchmark.cis_v130'
+
+steampipe query list
+
+# List AWS IAM users and their group
+steampipe query 'select name from aws_iam_role'
+steampipe query "SELECT iam_user ->> 'UserName' as User, name as Group FROM aws_iam_group CROSS JOIN jsonb_array_elements(users) as iam_user"
