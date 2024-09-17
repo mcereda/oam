5bdda874cf436a272f27a1c8108422c2b8432c19
Michele Cereda <cereda.michele@gmail.com>
2024-09-17 23:37:51 +0200

    test: try again with certificate creation using letsencrypt in pulumi

--- examples/pulumi/certificate from letsencrypt with dns01 challenge/index.ts
+++ examples/pulumi/certificate from letsencrypt with dns01 challenge/index.ts
@@ -1,36 +1,64 @@
-import * as acme from '@pulumiverse/acme';
+import * as acme from "@pulumiverse/acme";
+import * as aws from "@pulumi/aws";
 import * as cloudinit from "@pulumi/cloudinit";
 import * as pulumi from "@pulumi/pulumi";
 import * as tls from "@pulumi/tls";
 import * as yaml from "yaml";
+
+/**
+ * NOT WORKING
+ * Fails during certificate creation with errors "not found" and "response from server: EOF"
+ * It seems to find the DNS zone. It is like it does not find the DNS entry even if I create it manually?
+ **/
 
 
 /**
  * LetsEncrypt certificate - start
  * -------------------------------------
  * Leverage the DNS challenge to keep the instance private at all times.
+ * The private key *must be RSA* for ACME registration.
  **/
 
-const privateKey = new tls.PrivateKey(
+const acme_privateKey = new tls.PrivateKey(
     "privateKey",
     { algorithm: "RSA" },
 );
-const registration = new acme.Registration(
+const acme_registration = new acme.Registration(
     "registration",
     {
-        accountKeyPem: privateKey.privateKeyPem,
+        accountKeyPem: acme_privateKey.privateKeyPem,
         emailAddress: "example@company.com",
     },
 );
-const certificate = new acme.Certificate(
-    "certificate",
+const dnsRecord = new aws.route53.Record(
+    "gitlabDotCompanyDotcom",
     {
-        accountKeyPem: registration.accountKeyPem,
-        commonName: "gitlab.company.com",
-        dnsChallenges: [{
-            provider: "route53",
-        }],
+        zoneId: "ABCDEFGH01234",
+        name: "gitlab.company.com",
+        type: aws.route53.RecordType.A,
+        records: [ "127.0.0.1" ],
+        ttl: 300,
     },
+);
+const certificate = pulumi.all([ acme_privateKey.rsaBits, acme_registration.accountKeyPem ]).apply(
+    ([ keyType, accountKeyPem ]) => new acme.Certificate(
+        "gitlabDotCompanyDotcom",
+        {
+            commonName: dnsRecord.name,
+            minDaysRemaining: 10,
+            accountKeyPem: accountKeyPem,
+            keyType: keyType.toString(),
+            dnsChallenges: [{
+                provider: "route53",
+                config: {
+                    AWS_ACCESS_KEY_ID: "AKIA2HKHF01234567ABC",
+                    AWS_SECRET_ACCESS_KEY: "FfEeDdCcBbAa00/11223344556677889900aABcd",
+                    AWS_REGION: "eu-west-1",
+                    AWS_HOSTED_ZONE_ID: dnsRecord.zoneId,
+                },
+            }],
+        },
+    ),
 );
 
 /* LetsEncrypt certificate - end */
