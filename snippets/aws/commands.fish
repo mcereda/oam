c628cd295fd45d62c0780ee725392981f93a0e66
Michele Cereda <cereda.michele@gmail.com>
2024-10-09 22:18:30 +0200

    chore(aws/codedeploy): add command

--- snippets/aws/commands.fish
+++ snippets/aws/commands.fish
@@ -54,6 +54,9 @@ diff -y -W 200 \
 (aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'staging' --output json | psub) \
 (aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'production' --output 'json' | psub)
 
+aws deploy create-deployment --application-name 'Evidently' --deployment-group-name 'staging' \
+	--description 'This is Evidently a deployment (☞ﾟ∀ﾟ)☞' \
+	--s3-location 'bundleType=zip,bucket=deployments-bucket,key=evidently-master-staging.zip'
 
 
 ###
