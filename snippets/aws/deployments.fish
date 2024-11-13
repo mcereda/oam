d8e20315877ea672467a2a8c69ef36d9767b5a20
Michele Cereda <cereda.michele@gmail.com>
2024-11-13 01:28:18 +0100

    chore(snippets/aws): add listing deployments to be done

--- snippets/aws/deployments.fish
+++ snippets/aws/deployments.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 aws deploy list-applications
+
 aws deploy list-deployment-groups --application-name 'Evidently'
 aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'production' --output 'json' | pbcopy
 
@@ -11,3 +12,7 @@ diff -y -W 200 \
 aws deploy create-deployment --application-name 'Evidently' --deployment-group-name 'staging' \
 	--description 'This is Evidently a deployment (☞ﾟ∀ﾟ)☞' \
 	--s3-location 'bundleType=zip,bucket=deployments-bucket,key=evidently-master-staging.zip'
+
+aws deploy list-deployments --application-name 'Evidently' --deployment-group-name 'production' \
+	--include-only-statuses 'Created' 'Queued' 'InProgress'
+aws deploy get-deployment --deployment-id 'd-JNCR4R7F8'
