a4f7ac75e64746d064609947d1fc4cbb94e6ea5f
Michele Cereda <cereda.michele@gmail.com>
2025-01-20 23:25:23 +0100

    chore(aws): improve readability

--- snippets/aws/deployments.fish
+++ snippets/aws/deployments.fish
@@ -5,9 +5,9 @@ aws deploy list-applications
 aws deploy list-deployment-groups --application-name 'Evidently'
 aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'production' --output 'json' | pbcopy
 
-diff -y -W 200 \
-(aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'staging' --output json | psub) \
-(aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'production' --output 'json' | psub)
+diff -y -W '200' \
+	(aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'staging' --output 'json' | psub) \
+	(aws deploy get-deployment-group --application-name 'Evidently' --deployment-group-name 'production' --output 'json' | psub)
 
 aws deploy create-deployment --application-name 'Evidently' --deployment-group-name 'staging' \
 	--description 'This is Evidently a deployment (☞ﾟ∀ﾟ)☞' \
