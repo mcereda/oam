d8f7b8f19448ecf6cae8a6110d3df6b17ae2e078
Michele Cereda <cereda.michele@gmail.com>
2025-05-07 18:29:49 +0200

    chore(systemd): port time-limited command examples to snippets

--- snippets/systemd.sh
+++ snippets/systemd.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 sudo systemctl enable --now 'gitlab-runner'
+
 sudo journalctl -xefu 'gitlab-runner'
+sudo journalctl --since 'yesterday'
+sudo journalctl --since "2015-01-10 17:15:00"
+sudo journalctl --since '09:00' --until "1 hour ago"
+sudo journalctl --since "2015-01-10" --until "2015-01-11 03:00"
 
 sudo hostnamectl
 sudo hostnamectl status --static
