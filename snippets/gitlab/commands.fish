af4925433d264a20b4bf8128bdd43084f08eeed0
Michele Cereda <cereda.michele@gmail.com>
2025-09-11 20:09:27 +0200

    chore(gitlab): add rake command

--- snippets/gitlab/commands.fish
+++ snippets/gitlab/commands.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
+# Show information about the elasticsearch cluster for advanced search
 gitlab-rake 'gitlab:elastic:info'
+
+# List pending migrations
+gitlab-rake gitlab:elastic:list_pending_migrations
