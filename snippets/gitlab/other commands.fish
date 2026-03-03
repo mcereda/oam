dc04ea5390ba6238be500d4ea469b7926bd14ad6
Michele Cereda <cereda.michele@gmail.com>
2026-03-03 21:38:29 +0100

    chore: dump commands for future reference

--- snippets/gitlab/other commands.fish
+++ snippets/gitlab/other commands.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/env fish
 
 # List the current application settings of the GitLab instance.
-curl -sH 'PRIVATE-TOKEN: glpat-m-…' 'https://gitlab.fqdn/api/v4/application/settings' | jq
+curl -sH 'PRIVATE-TOKEN: glpat-abcdefghijklmnopqrst' 'https://gitlab.fqdn/api/v4/application/settings' | jq
 
 # Show the diff in settings between two instances
 jd -color \
 	( curl -ksH 'PRIVATE-TOKEN: glpat-abcdefghijklmnopqrst' 'https://gitlab.test.fqdn/api/v4/application/settings' | jq | psub ) \
 	( curl -ksH 'PRIVATE-TOKEN: glpat-0123456789abcdefghij' 'https://gitlab.prod.fqdn/api/v4/application/settings' | jq | psub)
+
+# Show information about project access tokens.
+curl -sH 'PRIVATE-TOKEN: glpat-abcdefghijklmnopqrst' 'https://gitlab.fqdn/api/v4/projects/58/access_tokens/300'
