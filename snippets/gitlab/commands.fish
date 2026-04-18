89110d08c96e2a50a914092c4d8e4fc36ad396e8
Michele Cereda <cereda.michele@gmail.com>
2026-04-18 14:05:07 +0200

    chore(gitlab/api): add commands

--- snippets/gitlab/commands.fish
+++ snippets/gitlab/commands.fish
@@ -5,3 +5,11 @@ gitlab-rake 'gitlab:elastic:info'
 
 # List pending migrations
 gitlab-rake gitlab:elastic:list_pending_migrations
+
+
+# List group wiki pages
+# `glab`` does not support group wikis.
+gitlab -o 'json' group-wiki list --group-id '42'
+
+# Get specific group wiki pages content by slug.
+gitlab -o 'json' group-wiki get --group-id '42' --slug 'runbooks/deploy-process' | jq -r '.content'
